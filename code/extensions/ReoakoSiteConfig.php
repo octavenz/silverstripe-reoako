f80088d7a3bb7178089fe084128c9fdaefd34e95
Dan Brosnan <dan.brosnan@octave.nz>
2021-10-11 16:19:27 +1300

    feat: api key heads up when set via env/yml

--- code/extensions/ReoakoSiteConfig.php
+++ code/extensions/ReoakoSiteConfig.php
@@ -7,9 +7,16 @@ use SilverStripe\Forms\TextField;
 
 use SilverStripe\ORM\DataExtension;
 
+use Octavenz\Reoako\Client\ReokakoClient;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Environment;
+
 
 /**
  * Extends {@see SiteConfig} to allow Reoako key to be set via the CMS
+ * 
+ * TODO: add subsite support
+ * 
  */
 class ReoakoSiteConfig extends DataExtension
 {
@@ -20,9 +27,40 @@ class ReoakoSiteConfig extends DataExtension
 
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->addFieldToTab(
-            "Root.Reoako",
-            new TextField("ReoakoAPI", "Reoako API Key")
-        );
+
+        $field = new TextField("ReoakoAPI", "Reoako API Key");
+        // Check environment as override
+        if ($envApiKey = Environment::getEnv('SS_REOAKO_API_KEY')) {
+            $field->setInputType("password");
+            $field->setReadonly(true);
+            $field->setRightTitle("Reoako API key set via ENV");
+            $fields->addFieldToTab(
+                "Root.Reoako",
+                $field
+            );
+            return;
+        }
+
+        // Key stored in YML
+        $key = Config::inst()->get(ReokakoClient::class, 'api_key');
+        if (!empty($key)) {
+            $field->setInputType("password");
+            $field->setReadonly(true);
+            $field->setRightTitle("Reoako API key set via YML");
+            $fields->addFieldToTab(
+                "Root.Reoako",
+                $field
+            );
+            return;
+        }
+
+        # Use stored API key from DB
+        if (empty($key)) {
+            $fields->addFieldToTab(
+                "Root.Reoako",
+                $field
+            );
+            return;
+        }
     }
 }
