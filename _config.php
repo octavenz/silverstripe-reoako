1482617da3bef6dbdc7dbc8b42b69ce7703818fb
Dan Brosnan <dan.brosnan@octave.nz>
2021-10-11 19:38:19 +1300

    feat: enable the reoako shortcode

--- _config.php
+++ _config.php
@@ -1,1 +1,5 @@
 <?php
+
+use SilverStripe\View\Parsers\ShortcodeParser;
+
+ShortcodeParser::get('default')->register('reoako', ['Octavenz\Reoako\Extensions\ReoakoShortCodeExtension', 'ReoakoShortCode']);
