a7ca66de3e98214ff515c6324d3c96db3c5993e5
Michal Kleiner <mk@011.nz>
2023-06-15 20:28:54 +1200

    Add uniqueness ID to FE JS requirement
    Ensure the template is added with a unique ID preventing it from being added multiple times.

--- code/extensions/ReoakoFrontendExtension.php
+++ code/extensions/ReoakoFrontendExtension.php
@@ -18,7 +18,7 @@ class ReoakoFrontendExtension extends DataExtension
             $vars = array(
                 "REOAKO_API_KEY" => $apiKey
             );
-            Requirements::javascriptTemplate("octavenz/reoako:dist/js/reoako-frontend.js", $vars);
+            Requirements::javascriptTemplate("octavenz/reoako:dist/js/reoako-frontend.js", $vars, 'reoako-fe-js');
         } else {
             Requirements::customScript('console.error("Reoako API key not set see README")');
         }
